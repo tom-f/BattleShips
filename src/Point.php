4c84042986fb8669834c710d423ed9ff8a7084a1
Tom Fawssett <tom.fawssett@gmail.com>
2015-09-20 17:11:26 +0100

    Add Hit test and hit state mutation to point class

--- src/Point.php
+++ src/Point.php
@@ -17,12 +17,36 @@ class Point
     /**
      * @var boolean
      */
-    private $isHit;
+    private $isHit = false;
 
     public function __construct($x, $y)
     {
         $this->x = $x;
         $this->y = $y;
+    }
+
+    private function setHit()
+    {
+        $this->isHit = true;
+    }
+
+    public function isHit()
+    {
+        return $isHit;
+    }
+
+    public function testHit($x, $y)
+    {
+        if ($this->isMe($x, $y)) {
+            $this->setHit();
+        }
+
+        return $this->isHit();
+    }
+
+    private function isMe($x, $y)
+    {
+        return (($x = $this->x) && ($y == $y));
     }
 
 }
