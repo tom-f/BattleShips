ddd6dcf255c6e516894c1bc1a299a177133563c6
Tom Fawssett <tom.fawssett@gmail.com>
2015-09-20 17:51:20 +0100

    Complete ship tests

--- tests/ShipTest.php
+++ tests/ShipTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use TomF\BattleShips\Ship;
+use TomF\BattleShips\Point;
 
 Class ShipTest extends PHPUnit_Framework_TestCase {
 
@@ -13,22 +14,34 @@ Class ShipTest extends PHPUnit_Framework_TestCase {
    public function testShipHitPercentage()
    {
        $ship = new Ship(4);
+       $ship->addCoordinate(new Point(1, 2));
+       $ship->addCoordinate(new Point(2, 2));
+       $ship->addCoordinate(new Point(3, 2));
+       $ship->addCoordinate(new Point(4, 2));
 
        $this->assertEquals(0, $ship->getHitPercentage());
 
        //Hit one
+       $ship->testHit(1, 2);
+
        $this->assertEquals(25, $ship->getHitPercentage());
 
        //Hit two
+       $ship->testHit(2, 2);
        $this->assertEquals(50, $ship->getHitPercentage());
 
        //Hit two more
+       $ship->testHit(3, 2);
+       $ship->testHit(4, 2);
        $this->assertEquals(100, $ship->getHitPercentage());
    }
 
    public function testShipNoHits()
    {
        $ship = new Ship(3);
+       $ship->addCoordinate(new Point(1, 2));
+       $ship->addCoordinate(new Point(2, 2));
+       $ship->addCoordinate(new Point(3, 2));
 
        $this->assertFalse($ship->anyHits());
    }
@@ -36,8 +49,12 @@ Class ShipTest extends PHPUnit_Framework_TestCase {
    public function testShipIsHit()
    {
        $ship = new Ship(3);
+       $ship->addCoordinate(new Point(1, 2));
+       $ship->addCoordinate(new Point(2, 2));
+       $ship->addCoordinate(new Point(3, 2));
 
        // Hit ship once
+       $ship->testHit(2, 2);
 
        $this->assertTrue($ship->anyHits());
    }
@@ -45,7 +62,12 @@ Class ShipTest extends PHPUnit_Framework_TestCase {
    public function testShipSunk()
    {
        $ship = new Ship(2);
+       $ship->addCoordinate(new Point(1, 2));
+       $ship->addCoordinate(new Point(2, 2));
+
        // Hit Twice;
+       $ship->testHit(1, 2);
+       $ship->testHit(2, 2);
 
        $this->assertTrue($ship->isSunk());
    }
